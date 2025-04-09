f83c54bf6fe0b3e209449398d9a953b72de9e147
ErrorC137 <154239085+ErrorC137@users.noreply.github.com>
2025-04-09 19:02:54 +0700

    Update Player.js

--- src/game/entities/Player.js
+++ src/game/entities/Player.js
@@ -1,89 +1,66 @@
 export default class Player extends Phaser.Physics.Arcade.Sprite {
     constructor(scene, x, y) {
-      super(scene, x, y, 'chef_red');
-      
-      scene.add.existing(this);
-      scene.physics.add.existing(this);
-      
-      // Player configuration
-      this.speed = 200;
-      this.carrying = null;
-      this.setCollideWorldBounds(true);
-      
-      // Input setup
-      this.cursors = scene.input.keyboard.createCursorKeys();
-      this.interactKey = scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
-      
-      // Animation setup
-      this.createAnimations();
+        super(scene, x, y, 'player');
+        scene.add.existing(this);
+        scene.physics.add.existing(this);
+        
+        this.carrying = null;
+        this.setCollideWorldBounds(true);
+        
+        // Pokémon-style movement
+        this.createAnimations();
+        this.setSize(16, 16).setOffset(8, 16);
     }
-  
+
     createAnimations() {
-      const anims = this.scene.anims;
-      
-      ['down', 'up', 'right', 'left'].forEach((direction, index) => {
-        anims.create({
-          key: `walk_${direction}`,
-          frames: anims.generateFrameNumbers('chef_red', {
-            start: index * 5,
-            end: index * 5 + 4
-          }),
-          frameRate: 10,
-          repeat: -1
+        this.anims.create({
+            key: 'walk',
+            frames: this.anims.generateFrameNumbers('player', { start: 0, end: 3 }),
+            frameRate: 10,
+            repeat: -1
         });
-      });
     }
-  
-    update() {
-      // Movement handling
-      const body = this.body;
-      body.setVelocity(0);
-  
-      if (this.cursors.left.isDown) {
-        body.setVelocityX(-this.speed);
-        this.anims.play('walk_left', true);
-      } else if (this.cursors.right.isDown) {
-        body.setVelocityX(this.speed);
-        this.anims.play('walk_right', true);
-      }
-  
-      if (this.cursors.up.isDown) {
-        body.setVelocityY(-this.speed);
-        this.anims.play('walk_up', true);
-      } else if (this.cursors.down.isDown) {
-        body.setVelocityY(this.speed);
-        this.anims.play('walk_down', true);
-      }
-  
-      // Interaction handling
-      if (Phaser.Input.Keyboard.JustDown(this.interactKey)) {
-        this.handleInteraction();
-      }
+
+    pickupItem(item) {
+        if (!this.carrying) {
+            this.carrying = item;
+            item.setVisible(false);
+            this.scene.physics.world.disableBody(item.body);
+        }
     }
-  
-    handleInteraction() {
-      if (this.carrying) {
-        this.dropItem();
-      } else {
-        this.pickUpItem();
-      }
-    }
-  
-    pickUpItem() {
-      const nearbyItems = this.scene.physics.overlapRect(
-        this.x - 32, this.y - 32, 64, 64, true
-      );
-      
-      if (nearbyItems.length > 0) {
-        this.carrying = nearbyItems[0].gameObject;
-        this.carrying.setVisible(false);
-        this.scene.hud.updateObjective('Item collected!');
-      }
-    }
-  
+
     dropItem() {
-      this.carrying.setPosition(this.x, this.y);
-      this.carrying.setVisible(true);
-      this.carrying = null;
+        if (this.carrying) {
+            this.carrying.setVisible(true);
+            this.carrying.body.enable = true;
+            this.carrying = null;
+        }
     }
-  }
+
+    update(keys) {
+        // Movement controls
+        this.body.setVelocity(0);
+        
+        if (keys.left.isDown) {
+            this.body.setVelocityX(-160);
+            this.anims.play('walk', true);
+            this.flipX = true;
+        } else if (keys.right.isDown) {
+            this.body.setVelocityX(160);
+            this.anims.play('walk', true);
+            this.flipX = false;
+        }
+        
+        if (keys.up.isDown) {
+            this.body.setVelocityY(-160);
+            this.anims.play('walk', true);
+        } else if (keys.down.isDown) {
+            this.body.setVelocityY(160);
+            this.anims.play('walk', true);
+        }
+        
+        if (this.body.velocity.x === 0 && this.body.velocity.y === 0) {
+            this.anims.stop();
+        }
+    }
+}
