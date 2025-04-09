7d3809920cad3cba83cd0a8801748bb17b0fa26d
ErrorC137 <154239085+ErrorC137@users.noreply.github.com>
2025-04-09 12:51:23 +0700

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,65 +1,20 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-
 module.exports = {
-  entry: './src/game/main.js',
+  entry: './src/main.js',
   output: {
-    filename: 'bundle.[contenthash].js',
     path: path.resolve(__dirname, 'docs'),
-    publicPath: '/CP-Delight-Kitchen-Chaos/',
-    globalObject: 'this'
+    filename: 'bundle.js'
   },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-            plugins: ['@babel/plugin-transform-runtime']
-          }
-        }
+        use: { loader: 'babel-loader' }
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif|ico)$/,
-        type: 'asset/resource',
-        generator: {
-          filename: 'assets/[hash][ext][query]'
-        }
+        test: /\.(png|json)$/,
+        type: 'asset/resource'
       }
     ]
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-      meta: {
-        'Content-Security-Policy': {
-          'http-equiv': 'Content-Security-Policy',
-          'content': [
-            "default-src 'self'",
-            "connect-src 'self' ws: wss: https://your-server.com",
-            "img-src 'self' data: blob: https://cdn.jsdelivr.net",
-            "script-src 'self' 'wasm-unsafe-eval' https://cdn.jsdelivr.net https://cdn.socket.io",
-            "style-src 'self' 'unsafe-inline'",
-            "worker-src 'self' blob:"
-          ].join('; ')
-        }
-      }
-    })
-  ],
-  resolve: {
-    extensions: ['.js'],
-    fallback: {
-      "fs": false,
-      "path": require.resolve("path-browserify")
-    }
-  },
-  optimization: {
-    minimize: true,
-    usedExports: true
   }
 };
